ffdc77f722ad7c2cdf272b6c3684536e1939d377
baconzitus <thiagolds50@gmail.com>
2020-09-02 22:16:15 -0300

    feat: atividade07

---
+++ atividade07.py
@@ -0,0 +1,9 @@
+carinho = ["GeForce-RTX-2080Ti","cooler_master","placa_mae","memoria_ram","hd","processador"]
+print("carinho: ",carinho)
+
+carinho.remove (input("oque vc deseja tirar: "))
+carinho.remove(input("oque vc deseja tirar: "))
+
+print("carinho: ",carinho)
+
+
