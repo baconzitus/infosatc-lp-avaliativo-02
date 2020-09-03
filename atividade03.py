ce15c70905df6664f0190804defc637dc60b9a32
baconzitus <thiagolds50@gmail.com>
2020-09-02 22:15:28 -0300

    feat: atividade03

---
+++ atividade03.py
@@ -0,0 +1,9 @@
+lista = [0,1,2,3,4,5,6,7,8,9]
+
+print(sum(lista))
+
+#ou
+total = 0           #se n declarar ele seja com o numero ou int ele desiste de viver
+for i in lista:
+    total+=i
+print(total)
