0da895f88212efb177ec18dfe445c73f9835badb
baconzitus <thiagolds50@gmail.com>
2020-09-02 22:16:07 -0300

    feat: atividade06

---
+++ atividade06.py
@@ -0,0 +1,9 @@
+filmes  = ["minions","minions2","minions3"]
+jogos   = ["jogo1","jogo2","jogo3"]
+livros   = ["livro1","livro2","livro3"]
+esportes= ["esporte","esporte2","esporte3"]
+
+lista = (filmes+jogos+livros+esportes)
+
+print(lista)
+print(livros[1])
